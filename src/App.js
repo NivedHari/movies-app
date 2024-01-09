4099b0cb6c6467820611a5902f87626f7b092a06
NivedHari <nivedhari44@gmail.com>
2024-01-09 14:56:26 +0530

    Fetch Movie on Page load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect,useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -10,47 +10,46 @@ function App() {
   let timeoutId;
 
  
-  async function fetchMoviesHandler() {
 
-    
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-  try {
-    const response = await fetch("https://swapi.dev/api/film/");
-    if(!response.ok){
-      throw new Error('Something went wrong ....Retrying');
-      
-    }
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong ....Retrying");
+      }
 
-    const data = await response.json();
+      const data = await response.json();
 
-    const tranformedMoves = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
+      const tranformedMoves = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
 
-    setMovies(tranformedMoves);
-    setIsLoading(false);
-    return;
-  } catch (error) {
+      setMovies(tranformedMoves);
+      setIsLoading(false);
+      return;
+    } catch (error) {
       setError(error.message);
       timeoutId = setTimeout(() => {
         fetchMoviesHandler();
       }, 5000);
-      
-      
-  }
-  
-}
-const cancelHandler = () => {
-  clearTimeout(timeoutId);
-  setIsLoading(false);
-  setError(error.message);
-};
+    }
+  },[]);
+  const cancelHandler = () => {
+    clearTimeout(timeoutId);
+    setIsLoading(false);
+    setError(error.message);
+  };
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  },[fetchMoviesHandler]);
 
   return (
     <React.Fragment>
@@ -58,16 +57,17 @@ const cancelHandler = () => {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-    {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-    {!isLoading && movies.length === 0 && !error && <p>Found no Movies</p>}
-    {!isLoading && error && <p>{error}</p> }
-    {isLoading && !error && <p>Loading...</p>}
-    <div>
-    {isLoading && error && <p>{error}</p> }
-    {error && isLoading &&  <button onClick={cancelHandler}>Cancel</button> }
-    </div>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found no Movies</p>}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && !error && <p>Loading...</p>}
+        <div>
+          {isLoading && error && <p>{error}</p>}
+          {error && isLoading && (
+            <button onClick={cancelHandler}>Cancel</button>
+          )}
+        </div>
       </section>
-      
     </React.Fragment>
   );
 }
